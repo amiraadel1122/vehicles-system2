a94be5129190a468d9058027a46d625e797c5be3
amiraadel1122 <amiraadel1122@gmail.com>
2021-04-07 18:00:57 +0200

    sidebar component

--- vehicles-system/src/Components/Sidebar/index.jsx
+++ vehicles-system/src/Components/Sidebar/index.jsx
@@ -1,10 +1,35 @@
 import React from 'react';
 import './styles.scss';
+import { Menu } from 'antd';
+import { UserOutlined, CarOutlined, FileTextOutlined } from '@ant-design/icons';
 
-const Sidebar = ()=> (
-    <div>
-        <h1>Amira and react</h1>
-    </div>
-);
+const Sidebar = () => {
+  const { SubMenu } = Menu;
+  return (
+    <Menu
+      mode="inline"
+      defaultSelectedKeys={['3']}
+      defaultOpenKeys={['sub1']}
+      className="side-menu"
+    >
+      <Menu.Item key="1" icon={<CarOutlined />}>
+        VEHICLE
+      </Menu.Item>
+      <SubMenu key="sub1" icon={<FileTextOutlined />} title="REPORT">
+        <Menu.Item key="2">Operating Cost</Menu.Item>
+        <Menu.Item key="3">Fuel History</Menu.Item>
+        <Menu.Item key="4">Total Cost</Menu.Item>
+        <Menu.Item key="5">Cost/Meter</Menu.Item>
+        <Menu.Item key="6">Expesne Summary</Menu.Item>
+        <Menu.Item key="7">Utilization</Menu.Item>
+        <Menu.Item key="8">Maintenance</Menu.Item>
+        <Menu.Item key="9">Service</Menu.Item>
+      </SubMenu>
+      <Menu.Item key="10" icon={<UserOutlined />}>
+        PEAPLE
+      </Menu.Item>
+    </Menu>
+  );
+};
 
 export default Sidebar;
--- vehicles-system/src/Containers/Vehicles/index.jsx
+++ vehicles-system/src/Containers/Vehicles/index.jsx
@@ -1,22 +1,42 @@
 import React from 'react';
-import { Layout } from 'antd';
-import Sidebar from '../../Components/Sidebar';
+import { Layout, Breadcrumb } from 'antd';
+import Logo from '../../Assets/images/logo.svg';
 import './styles.scss';
+import Sidebar from '../../Components/Sidebar';
 
 const Vehicles = () => {
-    const { Header, Sider, Content } = Layout;
-    return (
-        <Layout>
-        <Sider>
+  const { Header, Sider, Content } = Layout;
+  return (
+    <Layout>
+      <Header className="header">
+        <div className="logo">
+          <img src={Logo} alt="logo" />
+        </div>
+      </Header>
+      <Layout>
+        <Sider className="site-layout-background sider-container">
           <Sidebar />
         </Sider>
-        <Layout>
-          <Header>
-          </Header>
-          <Content>Content</Content>
+        <Layout style={{ padding: '0 24px 24px' }}>
+          <Breadcrumb style={{ margin: '16px 0' }}>
+            <Breadcrumb.Item>Home</Breadcrumb.Item>
+            <Breadcrumb.Item>List</Breadcrumb.Item>
+            <Breadcrumb.Item>App</Breadcrumb.Item>
+          </Breadcrumb>
+          <Content
+            className="site-layout-background"
+            style={{
+              padding: 24,
+              margin: 0,
+              minHeight: 280,
+            }}
+          >
+            Content
+          </Content>
         </Layout>
       </Layout>
-    )
-}
+    </Layout>
+  );
+};
 
 export default Vehicles;
